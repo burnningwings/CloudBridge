a795776f26700d1b2e67e6c08f4d3ef5acc9f93d
Gogary <gugugujiawei@qq.com>
2018-04-22 19:50:06 +0800

    更新部署方式

--- bin/start.sh
+++ bin/start.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 current_dir=`dirname "$0"`
+deploy_dir=$current_dir/../../deploy
+
+cd $deploy_dir
 
 # ssh到mu01进行端口转发，日志不输出到文件
 nohup ssh mu01 portforward :9992 cu03:9992 >/dev/null 2>&1 &
 
 echo "start to listen on mc.ccnl.scut.edu.cn:9992"
+
+java -jar CloudBridge-1.0-SNAPSHOT.jar --spring.config.location=$deploy_dir/config/application-prod.properties -Dlog4j.configuration=$deploy_dir/config/log4j.properties
+
---
+++ update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+current_dir=`dirname "$0"`
+
+git checkout .
+
+git pull origin master
+
+mvn clean && mvn package
+
+deploy_dir=$current_dir/../deploy
+
+if [ ! -d $deploy_dir ];
+then
+    mkdir -p $deploy_dir;
+fi
+
+cp target/CloudBridge-1.0-SNAPSHOT.jar $deploy_dir
